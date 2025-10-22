aa7aa31c02df8f348c88770fb87350b6d3609eb3
Tayyab Fayyaz <fayyaztayyab684@gmail.com>
2025-10-23 01:59:44 +0500

    edit middleware

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,10 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware } from "@clerk/nextjs/edge-middleware";
 
 export default clerkMiddleware();
 
-// 👇 Add this config
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
     '/(api|trpc)(.*)',
   ],
-  runtime: "nodejs", // <-- This line is the key fix
 };
